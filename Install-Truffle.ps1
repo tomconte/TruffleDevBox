9d5a3159ced1c30a7f3ea45401c3555265a2b1ba
Thomas Conte <thomas@conte.com>
2017-02-24 16:56:02 +0100

    Move npm to real global location

--- Install-Truffle.ps1
+++ Install-Truffle.ps1
@@ -8,6 +8,10 @@
 # Also need to make sure the execution policy allows running scripts that aren't code-signed
 # Set-ExecutionPolicy Unrestricted -Scope CurrentUser
 
+###
+### STEP 1: install generic tools, e.g. Node.JS and Git
+###
+
 # Download & install Node.JS
 
 Set-Location $env:TEMP
@@ -29,12 +33,16 @@ Start-Process $gitInstaller /silent -Wait
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
-# Install OpenSSL libraries -- required by secp256k1
-# We need the older 1.0.2 version that includes libeay32.lib
+###
+### STEP 2: prepare Windows to install native (compiled) Node modules
+###
 
-$openSSLInstaller = "Win64OpenSSL-1_0_2k.exe"
-Invoke-WebRequest -UseBasicParsing -Uri "https://slproweb.com/download/$openSSLInstaller" -OutFile $openSSLInstaller
-Start-Process $openSSLInstaller /verysilent -Wait
+# We need to instruct npm to install modules in a "real" global location instead of the user's %APPDATA% directory.
+# Otherwise everything will be wiped out by sysprep.
+
+mkdir C:\npm
+Set-Content -Path $ENV:ProgramFiles\nodejs\node_modules\npm\npmrc -Value prefix=C:\npm
+$env:Path += ";C:\npm"
 
 # Install Windows Build Tools
 # https://github.com/felixrieseberg/windows-build-tools
@@ -46,6 +54,12 @@ npm install --global windows-build-tools
 
 npm install --global npm@latest
 
+
+###
+### STEP 3: actually install the tools we are interested in, i.e. Truffle and TestRPC
+### TODO: lock the versions?
+###
+
 # Now we can finally install Truffle
 
 npm install --global truffle
@@ -54,11 +68,32 @@ npm install --global truffle
 
 npm install --global ethereumjs-testrpc
 
+###
+### STEP 4: install other tools, utilities and libraries
+###
+
 # Install VS Code
 
 $codeInstaller = "VSCodeSetup-stable.exe"
 Invoke-WebRequest -UseBasicParsing -Uri "https://vscode-update.azurewebsites.net/latest/win32/stable" -OutFile $codeInstaller
 Start-Process $codeInstaller "/verysilent /suppressmsgboxes /mergetasks=!runcode" -Wait
+
+# Install OpenSSL libraries -- required by "secp256k1 Node module
+# We NEED an older 1.0.2 version that includes libeay32.lib
+
+$openSSLInstaller = "Win64OpenSSL-1_0_2k.exe"
+Invoke-WebRequest -UseBasicParsing -Uri "https://slproweb.com/download/$openSSLInstaller" -OutFile $openSSLInstaller
+Start-Process $openSSLInstaller /verysilent -Wait
+
+###
+### STEP 5: final environment preparations
+###
+
+# Update the system path to include the new npm location
+
+$p = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+$p += ";C:\npm"
+[Environment]::SetEnvironmentVariable("Path", $p, [System.EnvironmentVariableTarget]::Machine)
 
 # The End
 
